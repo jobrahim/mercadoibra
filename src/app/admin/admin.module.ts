4756a85ed80a67bb2afcd78c1394e5a8dc9058be
jobrahim <jorgebastidascastellanos@gmail.com>
2020-08-21 10:01:53 -0300

    generar producto a través de formulario

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -12,6 +12,7 @@ import { TableComponent } from './components/table/table.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { FormProductComponent } from './components/form-product/form-product.component';
+import { ProductEditComponent } from './components/product-edit/product-edit.component';
 
 
 @NgModule({
@@ -21,7 +22,7 @@ import { FormProductComponent } from './components/form-product/form-product.com
     TableComponent, 
     DashboardComponent, 
     ProductsListComponent, 
-    FormProductComponent
+    FormProductComponent, ProductEditComponent
   ],
   imports: [
     CommonModule,
