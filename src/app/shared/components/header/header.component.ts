0c0e86ba4a74f9035d5a7ff635c7754a5c66ac71
jobrahim <jorgebastidascastellanos@gmail.com>
2020-08-23 10:39:29 -0300

    optimizando codigo header utilizando pipes

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {map} from 'rxjs/operators';
 import { CartService } from '../../../core/services/cart.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,15 +10,15 @@ import { CartService } from '../../../core/services/cart.service';
 })
 export class HeaderComponent implements OnInit {
 
-  total = 0;
+  total$: Observable<number>;
 
   constructor(
     private cartService: CartService
   ) { 
-    this.cartService.cart$.subscribe(products => {
-      console.log(products);
-      this.total = products.length;
-    });
+    this.total$ = this.cartService.cart$
+    .pipe(
+      map(products => products.length)
+    );
   }
 
   ngOnInit() {
