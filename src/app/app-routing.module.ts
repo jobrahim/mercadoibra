93ca15a63a816b1eea3e5b75f2fd3e3036ec74e0
jobrahim <jorgebastidascastellanos@gmail.com>
2020-08-28 12:34:07 -0300

    Implementado login y register components
    agregado registro en firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -43,7 +43,10 @@ const routes: Routes = [
     path: 'admin',
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
   },
-
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
   {
     path: '**',
     loadChildren: () => import('./pagenotfound/pagenotfound.module').then(m => m.PagenotfoundModule)
--- src/environments/environment.stag.ts
+++ src/environments/environment.stag.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  url_api: 'https://platzi-store-stag.herokuapp.com/'
+  url_api: 'https://platzi-store-stag.herokuapp.com/',
+  firebase: {
+  apiKey: "AIzaSyAS00fR6SBsh6TbifGyOnmDYWqp6Yok4Js",
+  authDomain: "mercadoibra.firebaseapp.com",
+  databaseURL: "https://mercadoibra.firebaseio.com",
+  projectId: "mercadoibra",
+  storageBucket: "mercadoibra.appspot.com",
+  messagingSenderId: "399069578101",
+  appId: "1:399069578101:web:4a38d2dddc63c79d40893c"
+  }
 };
