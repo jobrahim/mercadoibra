3254db4578d61d6c26c6601fc6b81338440d1e50
jobrahim <jorgebastidascastellanos@gmail.com>
2021-04-24 15:51:11 -0300

    corrección comillas

--- src/environments/environment.stag.ts
+++ src/environments/environment.stag.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   url_api: 'https://platzi-store-stag.herokuapp.com',
   firebase: {
-  apiKey: "AIzaSyAS00fR6SBsh6TbifGyOnmDYWqp6Yok4Js",
-  authDomain: "mercadoibra.firebaseapp.com",
-  databaseURL: "https://mercadoibra.firebaseio.com",
-  projectId: "mercadoibra",
-  storageBucket: "mercadoibra.appspot.com",
-  messagingSenderId: "399069578101",
-  appId: "1:399069578101:web:4a38d2dddc63c79d40893c"
+  apiKey: 'AIzaSyAS00fR6SBsh6TbifGyOnmDYWqp6Yok4Js',
+  authDomain: 'mercadoibra.firebaseapp.com',
+  databaseURL: 'https://mercadoibra.firebaseio.com',
+  projectId: 'mercadoibra',
+  storageBucket: 'mercadoibra.appspot.com',
+  messagingSenderId: '399069578101',
+  appId: '1:399069578101:web:4a38d2dddc63c79d40893c'
   }
 };
